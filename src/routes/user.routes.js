24510e6fa99f6adf0dca63d9b89b3a6621cdf03d
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-09-01 11:50:02 -0500

    feat: change module user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 const { verifyToken } = require('../middlewares/auth.middleware');
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
 router.get('/by-email', userController.getUserByEmail);
+router.get('/:id/status', verifyToken, userController.getUserStatus);
+
+router.get('/:id', userController.getUserById);
+router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
-router.get('/:id/status', verifyToken, userController.getUserStatus);
 router.patch('/strikes', userController.updateUserStrikes);
 
 module.exports = router;
