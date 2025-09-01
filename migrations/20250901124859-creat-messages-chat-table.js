a7a9ad80f726603e1cbfc4afce73a1fd0f35eee6
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-09-01 07:51:46 -0500

    feat: add table

---
+++ migrations/20250901124859-creat-messages-chat-table.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('messages_chat', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      userEmail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'email'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('messages_chat');
+  }
+};
